1caa7c24e5f2e3b96e0e1c6ed5df0336720d9256
asmaulhasnat <asmaulhasnat1311@gmail.com>
2021-04-25 08:06:15 +0600

    stable .env

--- app/core/db.php
+++ app/core/db.php
@@ -1,14 +1,15 @@
 <?php
 use Illuminate\Database\Capsule\Manager as Capsule;
-
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../');
+$dotenv->Load();
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'jwtauth',
-    'username'  => 'root',
-    'password'  => '',
+    'driver'    => $_ENV['DB_CONNECTION'] ?? 'mysql',
+    'host'      => $_ENV['DB_HOST'] ?? '127.0.0.1',
+    'database'  => $_ENV['DB_DATABASE'],
+    'username'  => $_ENV['DB_USERNAME'] ?? 'root',
+    'password'  =>$_ENV['DB_PASSWORD'],
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
